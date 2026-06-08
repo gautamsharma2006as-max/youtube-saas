c77449344698d006ad4c6b6d3425901d83186c29
yourname <you@example.com>
2026-06-09 01:02:47 +0530

    Fix next.config.ts configuration

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -10,4 +11,5 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
 
